279c81207adaac8122aa5840e8130c6cb3f3456c
Adelina Borozan <adelina.borozan24@gmail.com>
2026-05-18 19:40:16 +0300

    Initial commit

---
+++ src/test/java/UITests/DemoqaTests.java
@@ -0,0 +1,100 @@
+package UITests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class DemoqaTests {
+
+    private WebDriver driver;
+
+    @AfterMethod
+    public void inchideBrowser() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    @Test
+    public void textBoxTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/text-box");
+
+        String fullNameValue = "Adelina";
+
+        Actions actions = new Actions(driver);
+
+        WebElement fullName = driver.findElement(By.id("userName"));
+        WebElement email = driver.findElement(By.id("userEmail"));
+        WebElement currentAddress = driver.findElement(By.id("currentAddress"));
+        WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
+
+        WebElement submit = driver.findElement(By.className("btn-primary"));
+
+        fullName.sendKeys(fullNameValue);
+        email.sendKeys("test@itschool.com");
+        currentAddress.sendKeys("Timisoara");
+        permanentAddress.sendKeys("Timis");
+
+        actions.scrollToElement(submit).perform();
+        submit.click();
+
+
+        WebElement output = driver.findElement(By.id("output"));
+
+
+
+//        Assert.assertTrue(output.isDisplayed());
+
+
+        String outputName = driver.findElement(By.id("name")).getText();
+
+        System.out.println(outputName);
+
+        Assert.assertTrue(outputName.contains(fullNameValue), "Output name nu este corect");
+
+        Assert.assertTrue(false);
+
+    }
+
+    @Test
+    public void textBoxTestNegativ() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/text-box");
+
+        Actions actions = new Actions(driver);
+
+        WebElement fullName = driver.findElement(By.id("userName"));
+        WebElement email = driver.findElement(By.id("userEmail"));
+        WebElement currentAddress = driver.findElement(By.id("currentAddress"));
+        WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
+
+        WebElement submit = driver.findElement(By.className("btn-primary"));
+
+        fullName.sendKeys("Adelina");
+        email.sendKeys("test");
+        currentAddress.sendKeys("Timisoara");
+        permanentAddress.sendKeys("Timis");
+
+//        actions.scrollToElement(submit).perform();
+        submit.click();
+
+        String classAttribute = email.getAttribute("class");
+
+        System.out.println(classAttribute);
+        //comentariu la linia 78
+
+        Assert.assertTrue(classAttribute.contains("field-error"));
+
+        //acesta este un comentariu
+
+    }
+}
---
+++ src/test/java/listener/TestListener.java
@@ -0,0 +1,34 @@
+package listener;
+
+import io.qameta.allure.Allure;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+import utilities.DriverFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TestListener extends TestListenerAdapter {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+
+        WebDriver driver = DriverFactory.getDriver();
+
+        Path screenshotPath = utils.ScreenshotUtils.takeScreenshot(
+                driver,
+                result.getName()
+        );
+
+        if (screenshotPath != null) {
+            try (InputStream screenshot = Files.newInputStream(screenshotPath)) {
+                Allure.addAttachment(result.getName(), "image/png", screenshot, ".png");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
